8bd03791e9e4e7e205985ce071e3cc0ea4b8f07f
Yuni-Q <carnotcycle2@naver.com>
2019-04-13 13:51:24 +0900

    Add ColorCode Component

---
+++ src/components/ColorCode/ColorCode.js
@@ -0,0 +1,13 @@
+import style from './ColorCode.module.css'
+
+import React from 'react';
+
+const ColorCode = () => {
+  return (
+    <p className={style.wrapper}>
+      rgb(171, 67, 204)
+    </p>
+  )
+}
+
+export default ColorCode
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import style from './Game.module.css';
+import React from 'react'
+import style from './Game.module.css'
 import Score from '../Score'
+import ColorCode from '../ColorCode'
 
 const Game = () => {
   return (
     <div className={style.wrapper}>
       <Score />
+      <div className={style.centered}>
+        <ColorCode />
+      </div>
     </div>
   )
 }
